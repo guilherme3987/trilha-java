d90183757a2702773895c1e733f7cf6f9574cdf0
guilherme3987 <guilhermesmt3987@gmail.com>
2024-08-18 21:28:56 -0300

    Map

--- oop/Collections/lista_interface/ExampleHashMap.java
+++ oop/Collections/lista_interface/ExampleHashMap.java
@@ -3,6 +3,8 @@ package lista_interface;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.print.DocFlavor.INPUT_STREAM;
+
 public class ExampleHashMap {
     public static void main(String[] args) {
         Map<String,Integer> map = new HashMap<>();
@@ -10,5 +12,12 @@ public class ExampleHashMap {
         map.put("Apple",0);
         map.put("Banana", 1);
         
+        System.out.println(map+"\n\n");
+
+        for (Map.Entry<String, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + ":"+entry.getValue());
+        }
+
+
     }
 }
